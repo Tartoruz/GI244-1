5326298dc14b9d0c5bf7c555837bfc2a015819e0
Tarra <tartoruz01@gmail.com>
2024-01-31 12:41:51 +0700

    8.17

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -70,4 +70,11 @@ public class Unit : MonoBehaviour
     {
         
     }
+
+    public void ToggleSelectionVisual(bool flag)
+    {
+        if (selectionVisual != null)
+            selectionVisual.SetActive(flag);
+    }
+
 }
